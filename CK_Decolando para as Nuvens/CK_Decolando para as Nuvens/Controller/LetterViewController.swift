ee17b7e51aa0d399724cd960501fd9136d5255b4
Rebeca PachecoCorrea <rebecapc@icomp.ufam.edu.br>
2020-11-26 11:02:46 -0400

    feita a segunda classe de controller de tela

---
+++ CK_Decolando para as Nuvens/CK_Decolando para as Nuvens/Controller/LetterViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LetterViewController.swift
+//  CK_Decolando para as Nuvens
+//
+//  Created by Rebeca Pacheco on 26/11/20.
+//
+
+import UIKit
+
+class LetterViewController: UIViewController {
+    @IBOutlet weak var letterText: UITextView!
+    
+    @IBOutlet weak var signature: UITextField!
+    
+    @IBOutlet weak var ButtonLetter: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
