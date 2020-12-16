381743c69c6e52a9d3ab60934f31996ea57fdda3
Leonardo Viana <sonleo2013@gmail.com>
2020-12-15 22:44:56 -0400

    remove cloud kit code in model

--- CK_Decolando para as Nuvens/CK_Decolando para as Nuvens/Model/Letter.swift
+++ CK_Decolando para as Nuvens/CK_Decolando para as Nuvens/Model/Letter.swift
@@ -6,113 +6,34 @@
 //
 
 import Foundation
-import CloudKit
+//import CloudKit
 
 struct Letter{
     var name: String
     var content: String
-    var RecordId : CKRecord.ID? = nil
+//    var RecordId : CKRecord.ID? = nil
     
-    var container: CKContainer {
-        return CKContainer(identifier: "iCloud.br.com.WorkshopCK-Test")
-    }
+//    var container: CKContainer {
+//
+//    }
     
     func createRecord(completionHandler: @escaping (Error?)->()){
-        let record = CKRecord(recordType: "Letter")
-        
-        record["name"] = self.name as CKRecordValue
-        record["content"] = self.content as CKRecordValue
-    
-        container.publicCloudDatabase.save(record) { _, error in
-            if let error = error {
-                completionHandler(error)
-            } else {
-                completionHandler(nil)
-            }
-        }
+        //CODE HERE!
     }
     
     func readRecords(completionHandler: @escaping ([Letter]?,Error?)->()){
-        let predicate = NSPredicate(value: true)
-        let query = CKQuery(recordType: "Letter", predicate: predicate)
-        let operation = CKQueryOperation(query: query)
-        
-        var letterRecords: [Letter] = []
-        
-        operation.recordFetchedBlock = { record in
-            
-            let letter = Letter(name: record["name"] as! String, content: record["content"] as! String, RecordId: record.recordID)
-            
-            letterRecords.append(letter)
-        }
-        
-        operation.queryCompletionBlock = { (cursor, error) in
-            
-            if error == nil {
-                completionHandler(letterRecords,nil)
-            } else {
-                
-                completionHandler(nil,error)
-            }
-        }
-        
-        container.publicCloudDatabase.add(operation)
+        //CODE HERE!
     }
     
     func deleteRecord(completionHandler: @escaping (Error?)->()){
-        container.publicCloudDatabase.delete(withRecordID: self.RecordId!){
-            (_,error) in
-            if error == nil{
-                completionHandler(nil)
-            }else{
-                completionHandler(error)
-            }
-        }
+        //CODE HERE!
     }
     
     func updateRecord(name:String, content:String, completionHandler: @escaping (Error?)->()){
-        container.publicCloudDatabase.fetch(withRecordID: self.RecordId!){
-            (record,error) in
-            guard let record = record,error == nil else{
-                completionHandler(error!)
-                return
-            }
-            
-            record["name"] = name as CKRecordValue
-            record["content"] = content as CKRecordValue
-            
-            container.publicCloudDatabase.save(record) { _, error in
-                
-                if let error = error {
-                    completionHandler(error)
-                } else {
-                    completionHandler(nil)
-                }
-            }
-        }
+        //CODE HERE [CHALLENGE]!
     }
     
     func createSubscription(completionHandler: @escaping (Error?)->()){
-        let subscription = CKQuerySubscription(recordType: "Letter",
-                                               predicate: NSPredicate(value: true),
-                                               options: [.firesOnRecordCreation,.firesOnRecordUpdate,.firesOnRecordDeletion])
-       
-        let info = CKSubscription.NotificationInfo()
-        info.alertLocalizationKey = "letter_registered_alert"
-        info.alertLocalizationArgs = ["name"]
-        info.soundName = "pushSound.wav"
-        info.desiredKeys = ["name"]
-        info.alertBody = "nova carta criada"
-        info.shouldSendContentAvailable = true
-        subscription.notificationInfo = info
-        
-        container.publicCloudDatabase.save(subscription) { savedSubscription, error in
-            if let error = error {
-                completionHandler(error)
-            } else {
-                UserDefaults.standard.set(savedSubscription!.subscriptionID, forKey: "subscriptionID")
-                completionHandler(nil)
-            } 
-        }
+        //CODE HERE!
     }
 }
